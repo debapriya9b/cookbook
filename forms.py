9b2a9a3eca773b51e5ddda21efaf3391b92351af
Ubuntu <ubuntu@ip-172-31-83-2.ec2.internal>
2019-08-25 16:44:31 +0000

    Forms.py added,routes for register,login created in run.py,html pages created for registration,login

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -7,3 +7,10 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    
+    
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- run.py
+++ run.py
@@ -3,9 +3,13 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+from forms import RegistrationForm, LoginForm
 
 # declaring app name
 app = Flask(__name__)
+
+# Secret key got from secrets.token_hex()
+app.config['SECRET_KEY'] = '1645866b6820e28f1915bc58ebb62d53'
 
 app.config["MONGO_DBNAME"] = 'cookbook'
 app.config["MONGO_URI"] = 'mongodb+srv://debapriya9b:Chotolok10@myfirstcluster-bsyfh.mongodb.net/cookbook?retryWrites=true&w=majority'
@@ -22,6 +26,17 @@ def home():
 def get_recipes():
     return render_template('recipes.html',  recipes=mongo.db.recipes.find().sort('recipe_views', pymongo.DESCENDING).limit(4))
 
+#Registration
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+#Login
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__=='__main__':
     app.run(host=os.environ.get('IP'),

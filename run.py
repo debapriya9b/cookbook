be5401de4b4513dc20eb7709b9c9adf467234760
Ubuntu <ubuntu@ip-172-31-83-2.ec2.internal>
2019-08-29 11:58:06 +0000

    Register,Login page updated

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
-from forms import RegistrationForm, LoginForm
 
 # declaring app name
 app = Flask(__name__)
@@ -30,27 +29,73 @@ def add_recipe():
     return render_template('addrecipe.html',  recipes=mongo.db.recipes.find())    
     
     
-    
-
-@app.route("/register", methods=['GET', 'POST'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Welcome {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+    '''Accepts POST and GET requests.
+    If the request is a GET request, checks for a session email.
+    If session email is found, the homepage is rendered.
+    If no session email is found, the register template is rendered.
+    If the request is a POST request, form data is retrieved.
+    A check is performed to verify that the user email doesn't already exist.
+    If the user already exists, the register page is rendered with a user_exists error.
+    If a user does not exist, the user is added to the database and the login page is rendered.'''
 
+    # check for logged in user
+    email = session.get('email')
+    if email:
+      return redirect(url_for('home'))
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+    user = None
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        user = {'name': name, 'email': email, 'password': password}
+
+        if mongo.db.users.find_one({"email": email}):
+            return render_template('register.html', title='Register', error="user_exists")
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+            mongo.db.users.insert_one(user)
+            return render_template('login.html', title='Login', user=user, password=password)
+
+    return render_template('register.html', title='Register')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # check for logged in user
+    email = session.get('email')
+    if email:
+        return redirect(url_for('register'))
+
+    user = None
+    if request.method == 'POST':
+        email = request.form["email"]
+        user = mongo.db.users.find_one({"email": email})
+
+        try:
+            assert(user["password"] == request.form["password"])
+        except (AssertionError, TypeError):
+            return render_template('login.html', title='Login', user=None, error="incorrect_login")
+        else:
+            try:
+                session['name'] = user['name']
+            except KeyError:
+                session['name'] = 'John Doe'
+            session['email'] = email
+            return redirect(url_for("home"))
+
+    return render_template('login.html', title='Login', user=user)
+
+    
+    
+@app.route('/logout')
+def logout():
+    #  logout user and clear session
+    session['email'] = None
+    session['name'] = None
+    # session.clear()
+    return redirect(url_for('login'))
 
 
 
